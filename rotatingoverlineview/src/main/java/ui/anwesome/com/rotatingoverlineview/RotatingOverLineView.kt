cebcfebd378e923ded264447f337865a1ddbcfd2
Anwesh43 <anweshthecool0@gmail.com>
2018-01-28 01:18:19 +0530

    adding RotatingOverLineView

--- rotatingoverlineview/src/main/java/ui/anwesome/com/rotatingoverlineview/RotatingOverLineView.kt
+++ rotatingoverlineview/src/main/java/ui/anwesome/com/rotatingoverlineview/RotatingOverLineView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.rotatingoverlineview
 /**
  * Created by anweshmishra on 28/01/18.
  */
+import android.view.*
+import android.graphics.*
+import android.content.*
+class RotatingOverLineView(ctx:Context):View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas:Canvas) {
+
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
