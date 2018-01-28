2da2209de7fc54eb46a99fd7797a9ce6a32e2760
Anwesh43 <anweshthecool0@gmail.com>
2018-01-29 00:47:28 +0530

    adding RotatingLineListener in Activity

--- app/src/main/java/ui/anwesome/com/kotlinrotatinglineoverview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinrotatinglineoverview/MainActivity.kt
@@ -2,12 +2,18 @@ package ui.anwesome.com.kotlinrotatinglineoverview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ui.anwesome.com.rotatingoverlineview.RotatingOverLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        RotatingOverLineView.create(this)
+        val view = RotatingOverLineView.create(this)
+        view.addRotatingOverListener({j ->
+            Toast.makeText(this,"$j is opened",Toast.LENGTH_SHORT).show()
+        },{j ->
+            Toast.makeText(this,"$j is closed",Toast.LENGTH_SHORT).show()
+        })
     }
 }
